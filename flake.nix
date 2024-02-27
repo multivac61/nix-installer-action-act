265e08c77994d7a4e92a5ead80391c23c8ed6ba4
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-02-27 15:02:05 +0000

    Init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { lib, config, self', inputs', pkgs, system, ... }:
+        {
+          packages.default = pkgs.hello;
+        };
+    };
+}
+
